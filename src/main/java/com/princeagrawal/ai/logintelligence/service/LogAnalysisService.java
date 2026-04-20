c5aaef2efab32e605f4f2314fcf582568b9788e7
Prince Agrawal <princeagrawal024@gmail.com>
2026-04-20 22:09:28 +0530

    feat: implemented caching to improve performance and avoid unnecessary external API call

--- src/main/java/com/princeagrawal/ai/logintelligence/service/LogAnalysisService.java
+++ src/main/java/com/princeagrawal/ai/logintelligence/service/LogAnalysisService.java
@@ -4,13 +4,15 @@ import com.princeagrawal.ai.logintelligence.mapper.request.RequestMapper;
 import com.princeagrawal.ai.logintelligence.mapper.response.ResponseMapper;
 import com.princeagrawal.ai.logintelligence.model.response.LogResponseDto;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import static com.princeagrawal.ai.logintelligence.config.CacheConfig.LOGS_CACHE;
 
 @Service
 public class LogAnalysisService {
     private final RequestMapper requestMapper;
     private final ResponseMapper responseMapper;
-
     private final ChatClient chatClient;
 
     public LogAnalysisService(RequestMapper requestMapper, ResponseMapper responseMapper, ChatClient.Builder builder) {
@@ -18,7 +20,7 @@ public class LogAnalysisService {
         this.responseMapper = responseMapper;
         this.chatClient = builder.build();
     }
-
+    @Cacheable(LOGS_CACHE)
     public LogResponseDto analyzeLogs(String inputErrorLog) {
         String requestPrompt = requestMapper.mapInputErrorLogToRequestDto(inputErrorLog);
         String response = chatClient.prompt().user(requestPrompt).call().content();
